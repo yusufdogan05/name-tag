0549b35b40926ef53a2fb405fa4fcb4cb0b90501
yusufdogan05 <66487682+yusufdogan05@users.noreply.github.com>
2020-10-30 12:09:53 -0500

    LCD added

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 basic.forever(function () {
     basic.showString("Dogan")
     basic.showString("Hello!")
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # . # . .
+        # . # . .
+        # # . . .
+        `)
 })
